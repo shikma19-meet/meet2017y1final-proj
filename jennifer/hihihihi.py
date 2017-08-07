6a4152c72c355e71516dbd6a73cbc85c8c24c8b3
idane19-meet <idane19@meet.mit.edu>
2017-08-07 14:28:36 +0300

    Stopping here

--- jennifer/hihihihi.py
+++ jennifer/hihihihi.py
@@ -3,6 +3,7 @@ import random
 
 #lists
 box_color_list = ["box1.gif", "box2.gif", "box3.gif", "box4.gif", "box5.gif"]
+background_list = ["background1.gif", "background2.gif", "background3.gif", "background4.gif"]
 
 randombox = random.randint (0, len(box_color_list)-1)
 this_box = box_color_list[randombox]
@@ -13,21 +14,23 @@ turtle.register_shape(this_box)
 box.shape(this_box)
 
 
-turtle.mainloop ()
-
-
 background = random.randint (0,4)
 screen = turtle.Screen()
 
-####
 
-background_list = ["background1.gif", "background2.gif", "background3.gif", "background4.gif"]
+
 randbackground = random.randint (0,len(background_list)-1)
 this_background = background_list [randbackground]
+turtle.register_shape(this_background)
+turtle.bgpic (this_background)
 
-background = turtle.addshape(this_background)
-screen = turtle.Screen()
-turtle
 
-turtle.mainloop ()
-
+gabi = turtle.clone()
+gabi.penup()
+gabi.goto(0,-290)
+gabi.pendown()
+gabi.goto(0,285)
+gabi.penup()
+gabi.goto(350,0)
+gabi.pendown()
+gabi.goto(-340,0)
