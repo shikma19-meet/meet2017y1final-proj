81b384d17cf79e4ba4086944f0bcf3120cbaf116
idane19-meet <idane19@meet.mit.edu>
2017-08-07 21:48:49 +0200

    Stopping here

--- eat_it_idan.py
+++ eat_it_idan.py
@@ -10,7 +10,10 @@ def menu():
     elif x == 'YES':
         print('')
 menu()
+
 '''
+score = 0
+turtle.write(str(score) +": ", True, align="center")
 turtle.penup()
 #bird = turtle.clone()
 #turtle.addshape('bird.gif')
@@ -25,13 +28,14 @@ screen.bgcolor('light blue')
 turtle.goto(0,-200)
 good_food_pos= []
 bad_food_pos = []
-food_stamps = []
+good_food_stamps = []
+box_stamps = []
 box_pos=[]
 bird_pos=[]
 SIZE_X = 400
 SIZE_Y = 400
 turtle.setup(500,500)
-player_size = 100
+player_size = 10
 my_pos = turtle.pos()
 
 x_pos = my_pos[0]
@@ -63,13 +67,13 @@ def move_player():
     if x_pos >= RIGHT_EDGE:
             turtle.goto(RIGHT_EDGE - 10, y_pos)
     if x_pos <= LEFT_EDGE:
-            turtle.goto(LEFT_EDGE + 10, y_pos)
+            turtle.goto(LEFT_EDGE + 100, y_pos)
  
     if within_bounds: 
         if direction == RIGHT:
-            turtle.goto(x_pos + 10,y_pos)
+            turtle.goto(x_pos + 100,y_pos)
         elif direction == LEFT:
-            turtle.goto(x_pos - 10,y_pos)
+            turtle.goto(x_pos - 100,y_pos)
         '''
     else:
         # x checks
@@ -96,13 +100,16 @@ def move_player():
             elif direction == UP:
                 turtle.goto(x_pos, y_pos + 10)
     '''        
-    global food    
+    global food,score
     #turtle.ontimer(move_player,TIME_STEP)
     if turtle.pos() in good_food_pos:
         good_food_ind = good_food_pos.index(turtle.pos())
-        food.clearstamp(food_stamps[good_food_ind])
-        food_stamps.pop(good_food_ind)
+        food.clearstamp(good_food_stamps[good_food_ind])
+        good_food_stamps.pop(good_food_ind)
         good_food_pos.pop(good_food_ind)
+        print('EATEN')
+        score = score + 1
+        
         good_food()
 
 UP = 0
@@ -150,7 +157,6 @@ food.fillcolor('yellow')
 food.hideturtle()
 
 def good_food():
-    global good_pos, food
     min_x=-int(SIZE_X/2/player_size)+1
     max_x=int(SIZE_X/2/player_size)-1
     food_x = random.randint(min_x,max_x)*player_size
@@ -158,28 +164,27 @@ def good_food():
     good_food_pos.append(food.pos())
     stampnew = food.stamp()
     #stamp_old = food_stamps[-1]
-    food_stamps.append(stampnew)  
-
+    good_food_stamps.append(stampnew)
 box = turtle.clone()
-box.shape('square')
+box.shape('triangle')
 box.fillcolor('green')
 
-def box():
-    global y_pos
+def fall_box():
+    global y_pos,box
     x = random.randint(1,20)
     #box.goto(x,y_pos)
     box.goto(x,260)
     #box.addshape('box.gif')
     #box.shape('box.gif')
-
-def gravity():
-    global my_pos,x_box,y_box,y_pos,x_pos,x,box
     top_y = 260
-    #x_box = box.pos()[0]
-    #y_box = box.pos()[1]
+    all_way = 560
     for i in range(10):
-        top_y = top_y - 26
-        box.goto(x_pos,top_y)
+        top_y = top_y - 51
+        box.goto(x_pos,all_way)
+        new_stamp = box.stamp()
+        box_stamps.append(new_stamp)
+        box.clearstamp(-1)
+
 '''
 def bad_food():
     food = turtle.clone()
@@ -198,5 +203,5 @@ def bad_food():
 '''
 good_food()
 move_player()
-box()
-gravity()
+fall_box()
+
